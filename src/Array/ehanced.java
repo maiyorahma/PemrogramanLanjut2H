4e4d0e462864322db6792d8b9b59e5d12be95de9
ASUS <12050122022@students.uin-suska.ac.id>
2021-03-30 17:37:15 +0700

    materi

---
+++ src/Array/ehanced.java
@@ -0,0 +1,11 @@
+public class ehanced {
+    public static void main(String[] args) {
+        int[]arr={2,4};
+        int total =0;
+        for (int x:arr){
+            total +=x;
+
+            System.out.println(total);
+        }
+    }
+}
